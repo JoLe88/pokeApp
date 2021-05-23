463444cd6654e818087a4829cd826b5f48dc1daf
JoLe88 <jole.jerkovic@gmail.com>
2021-05-24 00:42:15 +0200

    Pokedex v1 - hide ActionBar in PokemonDetails

--- app/src/main/java/com/example/pokeapp/PokemonDetails.java
+++ app/src/main/java/com/example/pokeapp/PokemonDetails.java
@@ -42,7 +42,7 @@ public class PokemonDetails extends AppCompatActivity {
         getWindow().setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_pokedex);
+        setContentView(R.layout.activity_pokemon_details);
         getSupportActionBar().hide();
 
 
